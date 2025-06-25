838e28ea889a349266419b11b60b0220c2c00363
muneebjan07 <muneebahsan07@gmail.com>
2025-06-25 17:57:46 +0200

    refactor: adding more filter properties

--- lib/features/campsite_list/model/campsite_filter.dart
+++ lib/features/campsite_list/model/campsite_filter.dart
@@ -2,18 +2,34 @@ class CampsiteFilter {
   final bool? isCloseToWater;
   final bool? isCampFireAllowed;
   final List<String> selectedLanguages;
+  final double? minPrice;
+  final double? maxPrice;
+  final String searchKeyword;
 
-  const CampsiteFilter({this.isCloseToWater, this.isCampFireAllowed, this.selectedLanguages = const []});
+  const CampsiteFilter({
+    this.isCloseToWater,
+    this.isCampFireAllowed,
+    this.selectedLanguages = const [],
+    this.minPrice,
+    this.maxPrice,
+    this.searchKeyword = '',
+  });
 
-  CampsiteFilter copyWith({bool? isCloseToWater, bool? isCampFireAllowed, List<String>? selectedLanguages}) {
+  CampsiteFilter copyWith({
+    bool? isCloseToWater,
+    bool? isCampFireAllowed,
+    List<String>? selectedLanguages,
+    double? minPrice,
+    double? maxPrice,
+    String? searchKeyword,
+  }) {
     return CampsiteFilter(
       isCloseToWater: isCloseToWater ?? this.isCloseToWater,
       isCampFireAllowed: isCampFireAllowed ?? this.isCampFireAllowed,
       selectedLanguages: selectedLanguages ?? this.selectedLanguages,
+      minPrice: minPrice ?? this.minPrice,
+      maxPrice: maxPrice ?? this.maxPrice,
+      searchKeyword: searchKeyword ?? this.searchKeyword,
     );
   }
-
-  bool get isFiltering => isCloseToWater != null || isCampFireAllowed != null || selectedLanguages.isNotEmpty;
-
-  static const empty = CampsiteFilter();
 }
